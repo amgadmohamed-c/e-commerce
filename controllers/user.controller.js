78c7351b4070fd0013d2eb871c86bd49ac49bf90
mohamedamgad23@yahoo.com <amgad-mohamed>
2026-07-22 23:06:02 +0300

    user:added usercontroller

---
+++ controllers/user.controller.js
@@ -0,0 +1,27 @@
+
+const userService = require("../services/user.service");
+
+module.exports = {
+    async getUser(req, res) {
+        const user = await userService.getUser(req.user.id);
+        res.status(200).json(user);
+    },
+    async updateUser(req, res) {
+        try {
+            const { username, email } = req.body;
+            const updateduser = await userService.updateUser(req.user.id, { username, email });
+            res.status(200).json(updateduser);
+        }catch(err){
+            res.status(404).json({ message: 'User not found' });
+            return;
+        } },
+    async deleteUser(req, res) {
+        const { id } = req.params;
+        if (!id) {
+            res.status(400).json({ message: 'Please provide all the required fields' });
+            return;
+        }
+        const user = await userService.deleteUser(id);
+        res.status(200).json(user);
+    }
+};
