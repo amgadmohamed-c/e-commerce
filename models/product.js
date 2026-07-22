7da806d468145b725e9eee9d63a7ccd405f12a87
mohamedamgad23@yahoo.com <amgad-mohamed>
2026-07-22 13:08:32 +0300

    schema:added product

---
+++ models/product.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('product', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        }
+    }, {
+        sequelize,
+        tableName: 'products',
+        timestamps: false
+    });
+};
